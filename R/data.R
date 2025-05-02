2b86883bcac07d79ccdbd0873cbcbc39712d0f30
Matt Cheng <84427627+chengmatt@users.noreply.github.com>
2025-05-02 11:29:37 -0800

    try again ...

--- R/data.R
+++ R/data.R
@@ -6,14 +6,6 @@
 #' @source 2024 Federal Alaska Sablefish Assessment
 "sgl_rg_sable_data"
 
-#' Sablefish model results for single region case study
-#'
-#' A dataset containing all model results the Alaska sablefish single region case study.
-#'
-#' @format A list object containing parameters, report, and elements from a MakeADFun object
-#' @source 2024 Federal Alaska Sablefish Assessment
-"sgl_rg_sable_mod_results"
-
 #' Sablefish data for multi region case study
 #'
 #' A dataset containing the necessary elements for the Alaska sablefish spatial case study.
@@ -21,14 +13,6 @@
 #' @format A list with multiple components needed for the multi (5) region sablefish model
 #' @source Cheng et al. 2025, In review; Panmictic Panacea? Demonstrating Good Practices for Developing Spatial Stock Assessments through Application to Alaska Sablefish (Anoplopoma fimbria)
 "mlt_rg_sable_data"
-
-#' Sablefish model results for multi region case study
-#'
-#' A dataset containing all model results the Alaska sablefish multi region case study.
-#'
-#' @format A list object containing parameters, report, and elements from a MakeADFun object
-#' @source Cheng et al. 2025, In review; Panmictic Panacea? Demonstrating Good Practices for Developing Spatial Stock Assessments through Application to Alaska Sablefish (Anoplopoma fimbria)
-"mlt_rg_sable_mod_results"
 
 #' EBS Walleye Pollock data for single region case study
 #'
@@ -38,10 +22,3 @@
 #' @source 2024 Federal EBS Walleye Pollock Assessment
 "sgl_rg_ebswp_data"
 
-#' EBS Walleye Pollock model results for single region case study
-#'
-#' A dataset containing all model results the EBS Walleye Pollock single region case study.
-#'
-#' @format A list object containing parameters, report, and elements from a MakeADFun object
-#' @source 2024 Federal EBS Walleye Pollock Assessment
-"sgl_rg_ebswp_mod_results"
